366be777c627c95eba9abcf6b016d09f8e163085
izzyara <izzybzb@gmail.com>
2024-02-27 14:58:20 -0500

    Create Digits.java

---
+++ Digits.java
@@ -0,0 +1,15 @@
+public class Digits
+{
+        public static void main (String[] args)
+    {
+    int hundreds;
+    int tens;
+    int ones;
+    int num;
+
+    num = 258;
+    ones = num%10;
+    tens = num%10;
+
+    }
+}
