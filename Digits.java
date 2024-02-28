7473f5d8ecb8bb1ddb6aa6727bce1f5648dd44b3
izzyara <izzybzb@gmail.com>
2024-02-28 15:00:16 -0500

    Digits revised

--- Digits.java
+++ Digits.java
@@ -7,9 +7,13 @@ public class Digits
     int ones;
     int num;
 
-    num = 258;
+    num = 526;
     ones = num%10;
-    tens = num%10;
+    tens = (num/10)%10;
+    hundreds = (num-(tens*10)-(ones))/100;
 
+    System.out.println("the hundreds place digit is " + hundreds);
+    System.out.println("the tens place digit is " + tens);
+    System.out.println("the ones digit is " + ones);
     }
 }
